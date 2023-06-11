e6ba8baf3ee306b9eaebbfa348f80b1a8f0cc341
Parshant Singh <parshant.e2@gmail.com>
2023-06-11 03:30:49 +0200

    Add QR code

---
+++ pages/api/qr.ts
@@ -0,0 +1,23 @@
+import Cors from 'micro-cors';
+import type { NextApiRequest, NextApiResponse } from "next";
+import qr from "qr-image";
+const cors = Cors();
+
+
+export default cors(async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>,
+) {
+    const body = req.body
+    console.log("body: ", body);
+    // Convert JSON data to string
+    let dataString = JSON.stringify(body.credential);
+    let qr_png = qr.image(dataString, { type: "png" });
+
+    // Set the response headers
+    res.setHeader("Content-Disposition", "attachment; filename=qrcode.png");
+    res.setHeader("Content-Type", "image/png");
+
+    // Pipe the qr image to the response
+    qr_png.pipe(res);
+})
